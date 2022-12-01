1f4e4c5267960effbfc536382228b41499606a7e
CulinaryFrog <jahyung.yun@gmail.com>
2022-12-01 01:13:35 -0500

    Changed readme

--- Old School Projects/cs460-pa1/populate.sql
+++ Old School Projects/cs460-pa1/populate.sql
@@ -1,13 +1,14 @@
 USE photoshare2;
+
 DELETE FROM Users;
-INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Daniel', 'Zhou', 'dpz@bu.edu', '2000-07-28', 'dpz'); 
+INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Daniel', 'Zhou', 'dpz@bu.eduusersemail', '2000-07-28', 'dpz'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Bob', 'Melvin', 'bm@bu.edu', '2000-07-20','bm'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Mookie', 'Betts', 'mb@bu.edu', '2000-03-29','mb'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Rafael', 'Devers', 'rf@bu.edu', '2000-07-28','rf'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Chris', 'Sale', 'cs@bu.edu', '2000-07-28','cs'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Randy', 'Johnson', 'rj@bu.edu', '2000-07-28','rj'); 
 INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Alex', 'Cora', 'ac@bu.edu', '2000-07-28','ac'); 
-INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Alex', 'Verdugo', 'ac@bu.edu', '2000-07-28','av'); 
+#INSERT INTO Users (first_name, last_name, email, birth_date, password) VALUES ('Alex', 'Verdugo', 'ac@bu.edu', '2000-07-28','av'); 
 
 /*
 INSERT INTO Albums (albums_id, name, date, user_id) VALUES (1, 'a', '2000-06-28',3);
